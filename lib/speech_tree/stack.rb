0cfb662a1a869d45ce626c7eba18412c24ae04a2
Jamie Wright <jamie@brilliantfantastic.com>
2017-07-28 18:43:43 -0400

    Finished implementation of the stack

--- lib/speech_tree/stack.rb
+++ lib/speech_tree/stack.rb
@@ -1,19 +1,58 @@
 module SpeechTree
+  # Represents a Stack data type that responds to a minimal api.
+  # This is implemented as a linked list and follows LIFO ordering.
   class Stack
     def initialize(value=nil)
-      @node = value
+      @length = 0
+      push(value) unless value.nil?
+    end
+
+    def clear
+      @length = 0
+      @current = nil
     end
 
     def empty?
-      node.nil?
+      current.nil?
+    end
+
+    def length
+      @length
+    end
+
+    def peek
+      return nil if empty?
+      current.value
     end
 
     def push(value)
-      @node = value
+      @current = Node.new value, current
+      @length += 1
+    end
+    alias_method :<<, :push
+
+    def pop
+      raise StackEmptyError if empty?
+      value = current.value
+      @current = current.next_node
+      @length -= 1
+      value
     end
 
     private
 
-    attr_reader :node
+    attr_reader :current
+
+    class Node
+      attr_accessor :value, :next_node
+
+      def initialize(value, next_node)
+        @value = value
+        @next_node = next_node
+      end
+    end
+  end
+
+  class StackEmptyError < RuntimeError
   end
 end
--- spec/speech_tree/stack_spec.rb
+++ spec/speech_tree/stack_spec.rb
@@ -10,6 +10,19 @@ RSpec.describe SpeechTree::Stack do
   end
 
   describe "#clear" do
+    before { subject.push :blah }
+
+    it "clears the stack" do
+      subject.clear
+
+      expect(subject).to be_empty
+    end
+
+    it "resets the length to zero" do
+      subject.clear
+
+      expect(subject.length).to be_zero
+    end
   end
 
   describe "#empty?" do
@@ -25,8 +38,86 @@ RSpec.describe SpeechTree::Stack do
   end
 
   describe "#peek" do
+    context "with one item on the stack" do
+      before { subject.push :blah }
+
+      it "returns the value without removing the item from the stack" do
+        expect(subject.peek).to eq :blah
+      end
+
+      it "does not adjust the length" do
+        subject.peek
+
+        expect(subject.length).to eq 1
+        expect(subject).to_not be_empty
+      end
+    end
+
+    context "with an empty stack" do
+      it "returns nil" do
+        expect(subject.peek).to be_nil
+      end
+    end
   end
 
   describe "#pop" do
+    context "with one item on the stack" do
+      subject { described_class.new :blah }
+
+      it "returns the item" do
+        expect(subject.pop).to eq :blah
+      end
+
+      it "is now an empty stack" do
+        subject.pop
+
+        expect(subject).to be_empty
+      end
+
+      it "has a length of zero" do
+        subject.pop
+
+        expect(subject.length).to be_zero
+      end
+    end
+
+    context "with an empty stack" do
+      it "raises a StackEmptyError" do
+        expect { subject.pop }.to raise_error SpeechTree::StackEmptyError
+      end
+    end
+
+    context "with multiple items on the stack" do
+      before do
+        subject.push :first
+        subject.push :last
+      end
+
+      it "returns the last item on the stack" do
+        expect(subject.pop).to eq :last
+        expect(subject.pop).to eq :first
+      end
+
+      it "reduces the length of the stack" do
+        subject.pop
+
+        expect(subject.length).to eq 1
+      end
+    end
+  end
+
+  describe "#push" do
+    it "pushes the value onto the stack" do
+      subject.push :blah
+
+      expect(subject.length).to eq 1
+    end
+
+    it "pushes multiple values onto the stack" do
+      subject.push :blah
+      subject.push :bleh
+
+      expect(subject.length).to eq 2
+    end
   end
 end
